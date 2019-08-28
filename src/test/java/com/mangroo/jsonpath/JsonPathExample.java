0804d44a7903ebeca9cbdbcc748d5a4fad1fd6a3
Alan Mangroo <Alan.mangroo@elabor8.com.au>
2019-08-29 08:26:05 +1000

    Add logger

--- src/test/java/com/mangroo/jsonpath/JsonPathExample.java
+++ src/test/java/com/mangroo/jsonpath/JsonPathExample.java
@@ -3,18 +3,22 @@ package com.mangroo.jsonpath;
 import com.jayway.jsonpath.JsonPath;
 import com.mangroo.jsonpath.utils.FileLoader;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import static org.junit.Assert.assertEquals;
 
 public class JsonPathExample {
 
+    private Logger logger = LoggerFactory.getLogger(JsonPathExample.class);
+
     @Test
     public void extractValue() throws Throwable {
         FileLoader fileLoader = new FileLoader();
         String jsonString = fileLoader.loadFile("input.json");
 
-        System.out.println(jsonString);
+        logger.debug(jsonString);
 
         Object result = JsonPath.parse(jsonString)
                 .read("$.['name.blah']");
